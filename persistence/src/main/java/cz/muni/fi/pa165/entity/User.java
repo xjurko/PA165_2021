7ac3ef085b1a63e41ee82311de5f945943b1729c
lsolodkova <hsolodkova@gmail.com>
2021-04-03 18:56:07 +0200

    User entity done

--- persistence/src/main/java/cz/muni/fi/pa165/entity/User.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/User.java
@@ -1,6 +1,9 @@
 package cz.muni.fi.pa165.entity;
 
+import org.springframework.lang.NonNull;
+
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name="Users")
@@ -9,8 +12,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
+    @Column(nullable = false, unique=true)
     private String userName;
 
+    @NonNull
+    @Column(nullable = false, unique=true)
     private String email;
 
     public User(String userName, String email){
@@ -20,5 +27,27 @@ public class User {
 
     public Long getId() {
         return id;
+    }
+
+    public String getUserName() { return userName; }
+
+    public String getEmail() { return email; }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(!(o instanceof User)) return false;
+
+        final User u = (User) o;
+
+        if(!u.getUserName().equals(this.getUserName())) return false;
+        if(!u.getEmail().equals(this.getEmail())) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, email);
     }
 }
