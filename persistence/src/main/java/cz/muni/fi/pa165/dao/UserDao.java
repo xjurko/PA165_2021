7c5161d28fcda3b6b6fe3ed92451a53bc14c4e91
lsolodkova <hsolodkova@gmail.com>
2021-04-03 19:21:54 +0200

    UseDAO done; added findByName method - not sure if needed but it's strange to have only "by id" search - unit test needed

--- persistence/src/main/java/cz/muni/fi/pa165/dao/UserDao.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/UserDao.java
@@ -6,12 +6,46 @@ import cz.muni.fi.pa165.entity.User;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * DAO for operations with User entity.
+ * Fetches data from the table of registered users.
+ *
+ * @author lsolodkova
+ */
 public interface UserDao {
-    void store(User m);
+    /**
+     * stores or updates a single entity in the database
+     *
+     * @param u entity to be updated
+     */
+    void store(User u);
 
+    /**
+     * retrieves all stored entities
+     * @return all registered users
+     */
     List<User> fetchAll();
 
+    /**
+     * Find a particular user by id
+     *
+     * @param id entity's id
+     * @return User entity which matches the id
+     */
     Optional<User> findById(Long id);
 
+    /**
+     * Find a particular user by username
+     *
+     * @param name username
+     * @return User entity which matches the particular name
+     */
+    Optional<User> findByName(String name);
+
+    /**
+     * Removes a user with particular id from the database
+     *
+     * @param id user's id to be removed
+     */
     void remove(Long id);
 }
