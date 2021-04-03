e7ee2cfb8ed5be033a87a16984cc0057ef11a33a
Juraj <jurajj@txodds.com>
2021-04-03 22:41:29 +0200

    remove comment

--- persistence/src/main/java/cz/muni/fi/pa165/entity/RatingId.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/RatingId.java
@@ -7,10 +7,4 @@ import java.io.Serializable;
 public class RatingId implements Serializable {
     private Long movieId;
     private Long userId;
-
-//    public RatingId(Long userId, Long movieId) {
-//        super();
-//        this.userId = userId;
-//        this.movieId = movieId;
-//    }
 }
