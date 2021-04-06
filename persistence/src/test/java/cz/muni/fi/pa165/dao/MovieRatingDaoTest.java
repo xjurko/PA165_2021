b30ae70b69e29f5e7e749f67d64f2d8821a71e56
Juraj <jurajj@txodds.com>
2021-04-06 19:42:56 +0200

    movie rating dao test

---
+++ persistence/src/test/java/cz/muni/fi/pa165/dao/MovieRatingDaoTest.java
@@ -0,0 +1,108 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.PersistenceConfig;
+import cz.muni.fi.pa165.entity.Movie;
+import cz.muni.fi.pa165.entity.MovieRating;
+import cz.muni.fi.pa165.entity.User;
+import lombok.val;
+import org.hibernate.Session;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+
+/**
+ * Tests for MovieRatingDao
+ *
+ * @author juraj
+ */
+
+@ContextConfiguration(classes = PersistenceConfig.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class MovieRatingDaoTest extends AbstractTestNGSpringContextTests {
+
+    @Inject
+    private UserDao userDao;
+
+    @Inject
+    private MovieDao movieDao;
+
+    @Inject
+    private MovieRatingDao ratingDao;
+
+    @PersistenceContext
+    private EntityManager em;
+
+
+    @Test
+    public void testStoreRatingPreservesNewEntities() {
+        val user = new User("testUser1", "user1@fi.muni.cz");
+        val movie = new Movie("testMovie", Set.of(), Set.of(), 10, Set.of(), "", "");
+        val rating = new MovieRating(movie, user, 2);
+
+        ratingDao.store(rating);
+
+        em.flush();
+
+        val storedUser = userDao.findById(user.getId());
+        val storedMovie = movieDao.findById(movie.getId());
+
+        Assert.assertTrue(storedUser.isPresent());
+        Assert.assertEquals(storedUser, Optional.of(user));
+        Assert.assertTrue(storedUser.get().getMovieRatings().contains(rating));
+
+        Assert.assertTrue(storedMovie.isPresent());
+        Assert.assertEquals(storedMovie, Optional.of(movie));
+        Assert.assertTrue(storedMovie.get().getMovieRatings().contains(rating));
+
+    }
+
+
+    @Test
+    public void testRemoveRemovesFromLinkedEntities() {
+        val user = new User("testUser1", "user1@fi.muni.cz");
+        val movie = new Movie("testMovie", Set.of(), Set.of(), 10, Set.of(), "", "");
+
+        val rating = new MovieRating(movie, user, 2);
+        ratingDao.store(rating);
+        em.flush();
+
+        Assert.assertFalse(user.getMovieRatings().isEmpty());
+        Assert.assertFalse(movie.getMovieRatings().isEmpty());
+
+        ratingDao.remove(rating);
+
+        em.flush();
+
+        val storedMovie = movieDao.findById(movie.getId());
+        val storedUser = userDao.findById(user.getId());
+
+        Assert.assertTrue(user.getMovieRatings().isEmpty());
+        Assert.assertTrue(movie.getMovieRatings().isEmpty());
+
+        Assert.assertTrue(storedUser.isPresent());
+        Assert.assertEquals(storedUser.get().getMovieRatings(), user.getMovieRatings());
+        Assert.assertTrue(storedUser.get().getMovieRatings().isEmpty());
+
+
+        Assert.assertTrue(storedMovie.isPresent());
+        Assert.assertEquals(storedMovie.get().getMovieRatings(), movie.getMovieRatings());
+        Assert.assertTrue(storedMovie.get().getMovieRatings().isEmpty());
+    }
+}
