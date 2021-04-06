5db5532f244e0fdcbb656a0b72026845c0472016
Richard Šanda <sandarisa1@gmail.com>
2021-04-06 20:46:39 +0200

    adding director entity and dao + class diagram

--- persistence/src/main/java/cz/muni/fi/pa165/dao/DirectorDaoImpl.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/DirectorDaoImpl.java
@@ -1,35 +1,69 @@
 package cz.muni.fi.pa165.dao;
 
+import cz.muni.fi.pa165.entity.Actor;
 import cz.muni.fi.pa165.entity.Director;
+import cz.muni.fi.pa165.entity.Movie;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Director DAO implementation
+ *
+ * @author Richard Sanda
+ */
+
 @Repository
 public class DirectorDaoImpl implements DirectorDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
     public void store(Director d) {
-        throw new UnsupportedOperationException();
+        em.persist(d);
     }
 
     @Override
     public List<Director> fetchAll() {
-        throw new UnsupportedOperationException();
+        return em.createQuery("select d from Director d", Director.class)
+                .getResultList();
     }
 
     @Override
     public Optional<Director> findById(Long id) {
-        throw new UnsupportedOperationException();
+        try {
+            return Optional.ofNullable(
+                    em.createQuery("select d from Director d where d.id = :id", Director.class)
+                            .setParameter("id", id)
+                            .getSingleResult()
+            );
+
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<Director> findByName(String name) {
-        throw new UnsupportedOperationException();
+        try {
+            return Optional.ofNullable(
+                    em.createQuery("select d from Director d where d.name=:name", Director.class)
+                            .setParameter("name", name)
+                            .getSingleResult()
+            );
+
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public void remove(Director d1) {
-        throw new UnsupportedOperationException();
+        em.remove(d1);
     }
 }
--- persistence/src/main/java/cz/muni/fi/pa165/entity/Director.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/Director.java
@@ -1,35 +1,85 @@
 package cz.muni.fi.pa165.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Set;
+
+/**
+ * Actor entity
+ *
+ * @author Richard Sanda
+ */
 
 @Entity
 public class Director {
+
     @Id
-    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NonNull
+    @Column(nullable = false)
+    private String name;
 
     @ManyToMany
     private Set<Movie> movies;
 
-    public Director(String name) {
+    private LocalDate birthDate;
+
+
+    public Director(@NonNull String name) {
+        this.name = name;
     }
 
-    public Director() {
+    public Director() { }
 
+    public Director(@NonNull String name,
+                    Set<Movie> movies,
+                    LocalDate birthDate) {
+        this.name = name;
+        this.movies = movies;
+        this.birthDate = birthDate;
     }
 
     public void setName(String fullName) {
+        this.name = fullName;
     }
 
     public Long getId() {
-        throw new UnsupportedOperationException();
+        return this.id;
     }
 
     public String getName() {
-        throw new UnsupportedOperationException();
+        return this.name;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void addMovie(Movie movie){
+        this.movies.add(movie);
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Director)) return false;
+
+        Director director = (Director) o;
+
+        if (!name.equals(director.name)) return false;
+        return birthDate != null ? birthDate.equals(director.birthDate) : director.birthDate == null;
     }
 }
