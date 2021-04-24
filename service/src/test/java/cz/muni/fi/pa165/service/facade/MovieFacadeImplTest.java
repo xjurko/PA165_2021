8a090b1f28e450774df871cca2deea8fe5389163
Juraj <jurajj@txodds.com>
2021-04-24 21:02:16 +0200

    small test cleanup

--- service/src/test/java/cz/muni/fi/pa165/service/facade/MovieFacadeImplTest.java
+++ service/src/test/java/cz/muni/fi/pa165/service/facade/MovieFacadeImplTest.java
@@ -8,13 +8,10 @@ import cz.muni.fi.pa165.service.MovieService;
 import cz.muni.fi.pa165.service.config.ServiceConfig;
 import cz.muni.fi.pa165.service.converter.BeanConverter;
 import lombok.val;
-import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
@@ -33,36 +30,27 @@ import static org.mockito.Mockito.when;
 @ContextConfiguration(classes = ServiceConfig.class)
 public class MovieFacadeImplTest extends AbstractTransactionalTestNGSpringContextTests {
 
-    MovieService movieServiceMock = mock(MovieService.class);
-//    BeanConverter beanConverter = new DozerBeanConverter()
-//
+
     @Inject
     BeanConverter converter;
 
+    MovieService movieServiceMock = mock(MovieService.class);
+
     MovieFacade movieFacade;
-//
-//    @Inject
-//    @InjectMocks
-//    MovieFacade movieFacade;
 
     private Movie testMovie = new Movie("test", Set.of(), 0, Set.of(Genre.ACTION), "test caption", "test ref");
     private MovieDto testMovieDto = new MovieDto(null, "test", "test caption", 0, Set.of(), Set.of(), Set.of());
 
 
     @BeforeClass
-    public void setup()
-    {
-        MockitoAnnotations.initMocks(this);
-    }
-
-    @BeforeMethod
-    public void setup2(){
-        when(movieServiceMock.findMovieById(anyLong())).thenReturn(Optional.of(testMovie));
-        movieFacade = new MovieFacadeImpl(movieServiceMock, converter);
+    public void init(){
+//        movieFacade = new MovieFacadeImpl(movieServiceMock, converter);
     }
 
     @Test
     public void testFindMovieById() {
+        when(movieServiceMock.findMovieById(anyLong())).thenReturn(Optional.of(testMovie));
+
         Assert.assertNotNull(converter);
         val movie = movieFacade.findMovieById(123L);
         Assert.assertEquals(movie.get(), testMovieDto);
