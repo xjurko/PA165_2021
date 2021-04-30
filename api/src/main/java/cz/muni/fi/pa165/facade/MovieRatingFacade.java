a66d3556b59f2e2fd07651e42f94cf1009c244a7
phoenixday <a.tsykynovskaya@gmail.com>
2021-04-30 07:20:23 +0200

    draft MovieRating service

---
+++ api/src/main/java/cz/muni/fi/pa165/facade/MovieRatingFacade.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pa165.facade;
+
+/**
+ * @author alia
+ */
+public interface MovieRatingFacade {
+
+
+}
--- persistence/src/main/java/cz/muni/fi/pa165/entity/MovieRating.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/MovieRating.java
@@ -1,6 +1,7 @@
 package cz.muni.fi.pa165.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class MovieRating {
 
     @EmbeddedId
