29852d0383d560216b2fed00e84e4b1ec7403cd4
Juraj <jurajj@txodds.com>
2021-05-15 21:50:53 +0200

    capitalize genres

--- api/src/main/java/cz/muni/fi/pa165/dto/MovieDto.java
+++ api/src/main/java/cz/muni/fi/pa165/dto/MovieDto.java
@@ -2,11 +2,12 @@ package cz.muni.fi.pa165.dto;
 
 import lombok.*;
 
+import java.util.List;
 import java.util.Set;
 
 /**
  * @author juraj
- *
+ * <p>
  * DTO to be used as a return type when retrieving movies
  */
 
@@ -23,4 +24,5 @@ public class MovieDto {
     Set<DirectorDto> directors;
     String externalRef;
     String posterUrl;
+    List<Genre> genres;
 }
