3bcd85f3e3b9135bc2fdc08e5b3ecfc059c08e1f
Juraj <jurajj@txodds.com>
2021-05-15 18:59:14 +0200

    init rest module

---
+++ rest/src/main/java/cz/muni/fi/pa165/rest/controller/MovieController.java
@@ -0,0 +1,61 @@
+package cz.muni.fi.pa165.rest.controller;
+
+import cz.muni.fi.pa165.dto.MovieDto;
+import cz.muni.fi.pa165.facade.MovieFacade;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author juraj
+ */
+
+@RestController
+@AllArgsConstructor
+public class MovieController {
+
+    MovieFacade moviesFacade;
+
+    @GetMapping("/movie/{id}")
+    Optional<MovieDto> getMovie(@PathVariable Long id) {
+        return moviesFacade.getMovieById(id);
+    }
+
+
+//    @GetMapping("/employees/{id}")
+//    Employee one(@PathVariable Long id) {
+//
+//        return repository.findById(id)
+//            .orElseThrow(() -> new EmployeeNotFoundException(id));
+//    }
+//
+//    @PutMapping("/employees/{id}")
+//    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+//
+//        return repository.findById(id)
+//            .map(employee -> {
+//                employee.setName(newEmployee.getName());
+//                employee.setRole(newEmployee.getRole());
+//                return repository.save(employee);
+//            })
+//            .orElseGet(() -> {
+//                newEmployee.setId(id);
+//                return repository.save(newEmployee);
+//            });
+//    }
+//
+//    @DeleteMapping("/employees/{id}")
+//    void deleteEmployee(@PathVariable Long id) {
+//        repository.deleteById(id);
+//    }
+}
+
