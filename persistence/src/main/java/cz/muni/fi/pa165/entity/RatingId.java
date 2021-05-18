617fdd5011b532ff5b0ffe4466cd5553bd76089d
Juraj <jurajj@txodds.com>
2021-05-18 21:31:05 +0200

    fix recomendations for user, fix genres data

--- persistence/src/main/java/cz/muni/fi/pa165/entity/RatingId.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/RatingId.java
@@ -1,9 +1,12 @@
 package cz.muni.fi.pa165.entity;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
+@NoArgsConstructor
 public class RatingId implements Serializable {
     private Long movieId;
     private Long userId;
