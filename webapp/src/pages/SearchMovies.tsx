1b39abbaf84c5623510fbccb2fde0186744ca8e6
sandarisa1@gmail.com <Nem2rotVot>
2021-05-25 23:53:46 +0200

    initial commit

--- webapp/src/pages/SearchMovies.tsx
+++ webapp/src/pages/SearchMovies.tsx
@@ -1,87 +1,109 @@
 import {
-    IonContent,
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonChip, IonCol,
+    IonContent, IonGrid,
     IonHeader,
-    IonPage,
+    IonIcon,
+    IonImg, IonInfiniteScroll, IonInfiniteScrollContent,
+    IonLabel,
+    IonList,
+    IonPage, IonRow, IonSearchbar,
     IonTitle,
     IonToolbar,
-    IonSearchbar,
-    IonButton,
-    IonIcon
+    useIonViewWillEnter
 } from '@ionic/react';
-import {IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent} from '@ionic/react';
-import {useIonViewWillEnter} from '@ionic/react';
-import {IonInfiniteScroll, IonInfiniteScrollContent} from '@ionic/react';
-import {IonGrid, IonRow, IonCol} from '@ionic/react';
-import {search} from 'ionicons/icons'
-
-import './Home.css';
-
-import React, {useState} from "react"
+import {home, search, thumbsDownOutline, thumbsUp} from 'ionicons/icons'
+import './MovieDetails.css';
+import {RouteComponentProps} from "react-router";
 import {Link} from 'react-router-dom';
+import React, {useState} from "react";
 
-interface Movie {
-    id: number
-    name: string
-    releaseYear: number
-    caption: string
-    posterUrl: string
-    runtimeMin: number
-    genres: string[]
-}
+type Actor = {
+    id: number;
+    fullName: string;
+    posterUrl: string;
+};
 
-const Home: React.FC = () => {
+type Director = {
+    id: number;
+    name: string;
+    posterUrl: string;
+};
+
+type Movie = {
+    id: number;
+    name: string;
+    caption: string;
+    img: string;
+    releaseYear: number;
+    runtimeMin: number;
+    cast: Actor[];
+    directors: Director[];
+    externalRef: string;
+    genres: string[];
+    posterUrl: string;
+};
+
+interface SearchMoviesProps extends RouteComponentProps<{
+    str: string;
+}> {}
+
+const SearchMovies: React.FC<SearchMoviesProps> = ({match}) => {
     const [movies, setMovies] = useState<Movie[]>([])
     const [page, setPage] = useState(1)
     const [searchText, setSearchText] = useState('');
 
     const [disableInfiniteScroll, setDisableInfiniteScroll] = useState<boolean>(false);
 
-    const getMovies = async (page: number) => {
+    const getMovies = async () => {
         try {
-            const response: Response = await fetch("http://localhost:5000/movies/" + page)
+            const response: Response = await fetch("http://localhost:5000/movie/find/" + match.params.str)
             if (!response.ok) {
                 throw Error(response.statusText)
             }
             const json = await response.json()
             setMovies(movies.concat(json))
-            setDisableInfiniteScroll(json.length < 10);
             console.log(json)
         } catch (error) {
             console.error(error.message)
         }
     }
 
-    async function searchNext($event: CustomEvent<void>) {
-        setPage(page + 1)
-        await getMovies(page);
-
-        ($event.target as HTMLIonInfiniteScrollElement).complete();
-    }
-
     // hook to fetch data and display them on page display
     useIonViewWillEnter(async () => {
-        setPage(page + 1)
-        await getMovies(page);
+        //setPage(page + 1)
+        await getMovies();
     });
 
     return (
         <IonPage>
             <IonHeader>
+
+
                 <IonToolbar>
+                    <IonButton color="light" fill="solid" routerLink="/">
+                        <IonIcon icon={home}/>
+                    </IonButton>
                     <IonTitle>Catalogue</IonTitle>
                 </IonToolbar>
+
             </IonHeader>
 
             <IonContent>
-                <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} animated placeholder="Filter Movies" >
-                    <IonButton routerLink={"/find/" + searchText}>
+                <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)}>
+                    <IonButton slot="end" routerLink={"/find/" + searchText} >
                         <IonIcon icon={search} />
                     </IonButton>
                 </IonSearchbar>
 
-
                 <IonHeader collapse="condense">
                     <IonToolbar>
+
                         <IonTitle size="large">Movie Recommender</IonTitle>
                     </IonToolbar>
                 </IonHeader>
@@ -107,16 +129,9 @@ const Home: React.FC = () => {
                         </IonCard>
                     </Link>
                 ))}
-                <IonInfiniteScroll threshold="1600px"
-                                   disabled={disableInfiniteScroll} //this threshold will need to be change d if the card size changes
-                                   onIonInfinite={(e: CustomEvent<void>) => searchNext(e)}>
-                    <IonInfiniteScrollContent
-                        loadingText="Loading...">
-                    </IonInfiniteScrollContent>
-                </IonInfiniteScroll>
             </IonContent>
         </IonPage>
     );
 };
 
-export default Home;
+export default SearchMovies;
