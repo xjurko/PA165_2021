8258071bd3a335c73640f84dc8e5edbc03638151
Juraj Jurkovic <jurajj@txodds.com>
2021-05-29 09:13:57 +0200

    implement setting rating from movie detail card

--- webapp/src/pages/MovieDetails.tsx
+++ webapp/src/pages/MovieDetails.tsx
@@ -1,161 +1,254 @@
 import {
-    IonButton,
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
-    IonCardSubtitle,
-    IonCardTitle,
-    IonChip,
-    IonContent,
-    IonHeader,
-    IonIcon,
-    IonImg,
-    IonLabel,
-    IonList,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-    useIonViewWillEnter
+	IonButton,
+	IonCard,
+	IonCardContent,
+	IonCardHeader,
+	IonCardSubtitle,
+	IonCardTitle,
+	IonChip,
+	IonContent,
+	IonHeader,
+	IonIcon,
+	IonImg,
+	IonLabel,
+	IonList,
+	IonPage,
+	IonTitle,
+	IonToolbar, useIonModal,
+	useIonViewWillEnter
 } from '@ionic/react';
-import {home, thumbsDownOutline, thumbsUp} from 'ionicons/icons'
+import {home, thumbsDown, thumbsDownOutline, thumbsUp, thumbsUpOutline} from 'ionicons/icons'
 import './MovieDetails.css';
 import {RouteComponentProps} from "react-router";
 import {Link} from 'react-router-dom';
 import {useState} from "react";
+import {normalizeGenre, normalizeRuntime} from "../utils";
+import {LoginCard} from "../components/LoginCard";
 
-type Actor = {
-    id: number;
-    fullName: string;
-    posterUrl: string;
+export type Actor = {
+	id: number;
+	fullName: string;
+	posterUrl: string;
 };
 
-type Director = {
-    id: number;
-    name: string;
-    posterUrl: string;
+export type Director = {
+	id: number;
+	name: string;
+	posterUrl: string;
 };
 
 type Movie = {
-    id: number;
-    name: string;
-    caption: string;
-    img: string;
-    releaseYear: number;
-    runtimeMin: number;
-    cast: Actor[];
-    directors: Director[];
-    externalRef: string;
-    genres: string[];
-    posterUrl: string;
+	id: number;
+	name: string;
+	caption: string;
+	img: string;
+	releaseYear: number;
+	runtimeMin: number;
+	cast: Actor[];
+	directors: Director[];
+	externalRef: string;
+	genres: string[];
+	posterUrl: string;
 };
+
+const liked = 1
+const disliked = 2
+const notRated = 0
 
 interface MovieDetailsProps extends RouteComponentProps<{
-    id: string;
-}> {}
+	id: string;
+}> {
+}
 
 const MovieDetails: React.FC<MovieDetailsProps> = ({match}) => {
-    const [movie, setMovie] = useState<Movie>({
-        id: 1, name: "", caption: "", img: "", releaseYear: 1,
-        runtimeMin: 1, cast: [], directors: [], externalRef: "", genres: [], posterUrl: ""
-    })
-    const [recommended, setRecommendedMovies] = useState<Movie[]>([])
+		const [movie, setMovie] = useState<Movie>({
+			id: 1, name: "", caption: "", img: "", releaseYear: 1,
+			runtimeMin: 1, cast: [], directors: [], externalRef: "", genres: [], posterUrl: ""
+		})
+		const [recommended, setRecommendedMovies] = useState<Movie[]>([])
 
-    useIonViewWillEnter(() => {
-        console.log("enter new movie page")
-        fetch("http://localhost:5000/movie/" + match.params.id)
-            .then(resp => resp.json())
-            .then(data => setMovie(data))
-
-
-        fetch("http://localhost:5000/movie/recommend/" + match.params.id)
-            .then(resp => resp.json())
-            .then(data => setRecommendedMovies(data))
+		useIonViewWillEnter(() => {
+			fetchRating()
+			console.log("enter new movie page")
+			fetch("http://localhost:5000/movie/" + match.params.id)
+				.then(resp => resp.json())
+				.then(data => setMovie(data))
 
 
-    });
+			fetch("http://localhost:5000/movie/recommend/" + match.params.id)
+				.then(resp => resp.json())
+				.then(data => setRecommendedMovies(data))
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButton color="light" fill="solid" routerLink="/">
-                        <IonIcon icon={home}/>
-                    </IonButton>
-                    <IonTitle>{movie.name} ({movie.releaseYear})</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonCard>
-                    <IonCardHeader>
-                        <IonCardSubtitle
-                            class="card-subtitle">{movie.releaseYear} | {movie.runtimeMin}min</IonCardSubtitle>
-                        <IonCardTitle>{movie.name}</IonCardTitle>
-                    </IonCardHeader>
-                    <IonImg src={movie.posterUrl}/>
-                    <IonCardContent>
-                        {movie.genres.map((genre, i) => (
-                            <IonChip outline key={i}>
-                                <IonLabel>{genre}</IonLabel>
-                            </IonChip>
-                        ))}
-                        <p>{movie.caption}</p>
-                        <IonIcon icon={thumbsUp} size="large"/>
-                        <IonIcon icon={thumbsDownOutline} size="large"/>
-                    </IonCardContent>
-                </IonCard>
-                <IonCard>
-                    <IonCardHeader>
-                        <IonCardSubtitle>Cast</IonCardSubtitle>
-                    </IonCardHeader>
-                    <IonCardContent>
-                        <IonList class="lst">
-                            {movie.cast.map((actor, i) => (
-                                <Link to={"/actor/".concat(String(actor.id))} key={i}>
-                                    <div className="itm">
-                                        <IonImg src={actor.posterUrl} class="img"/>
-                                        <IonLabel>{actor.fullName}</IonLabel>
-                                    </div>
-                                </Link>
-                            ))}
-                        </IonList>
-                    </IonCardContent>
-                </IonCard>
-                <IonCard>
-                    <IonCardHeader>
-                        <IonCardSubtitle>Directors</IonCardSubtitle>
-                    </IonCardHeader>
-                    <IonCardContent>
-                        <IonList class="lst">
-                            {movie.directors.map((director, i) => (
-                                <div className="itm" key={i}>
-                                    <IonImg src={director.posterUrl} class="img"/>
-                                    <IonLabel>{director.name}</IonLabel>
-                                </div>
-                            ))}
-                        </IonList>
-                    </IonCardContent>
-                </IonCard>
-                <IonCard>
-                    <IonCardHeader>
-                        <IonCardSubtitle>Recommended Movies</IonCardSubtitle>
-                    </IonCardHeader>
-                    <IonCardContent>
-                        <IonList class="lst">
-                            {recommended.length ? (recommended.map((mov, i) => (
-                                <Link to={"/movie/".concat(String(mov.id))} key={i}>
-                                    <div className="itm">
-                                        <IonImg src={mov.posterUrl} class="img"/>
-                                        <IonLabel>{mov.name}</IonLabel>
-                                    </div>
-                                </Link>
-                            ))) : <div>No movies found.</div>
-                            }
-                        </IonList>
-                    </IonCardContent>
-                </IonCard>
-            </IonContent>
-        </IonPage>
-    );
-};
+		});
+
+
+		const handleRatingClick = (clickedValue: number) => {
+			if (clickedValue === rating) deleteRating()
+			else postRating(clickedValue)
+		}
+
+		const postRating = function (clicked: number) {
+			const token = localStorage.getItem('currentUser') || ""
+			const req = {
+				method: 'POST',
+				headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+			};
+
+			const newRating = clicked === liked ? 'LIKED' : 'DISLIKED'
+
+			return fetch(`http://localhost:5000/user/ratings/${match.params.id}/${newRating}`, req)
+				.then(resp => {
+						if (resp.ok) {
+							setRating(clicked)
+						} else if (resp.status === 401 || resp.status === 403) {
+							spawnModal({
+								cssClass: 'auto-height',
+								swipeToClose: true
+							})
+						}
+					}
+				)
+		}
+		const handleDismiss = () => {
+			dismissModal();
+		};
+
+		const [spawnModal, dismissModal] = useIonModal(LoginCard, {
+			dismiss: handleDismiss,
+			promptMessage: "Login to continue"
+		});
+
+
+		const deleteRating = () => {
+			const token = localStorage.getItem('currentUser') || ""
+			const req = {
+				method: 'DELETE',
+				headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+			};
+
+			fetch(`http://localhost:5000/user/ratings/${match.params.id}`, req).then(resp => {
+					if (resp.ok) setRating(notRated)
+				}
+			)
+		}
+
+		const fetchRating = function () {
+			const token = localStorage.getItem('currentUser') || ""
+			const req = {
+				method: 'GET',
+				headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${token}`},
+			};
+
+			return fetch(`http://localhost:5000/user/ratings/${match.params.id}`, req)
+				.then(resp => {
+					if (resp.ok) {
+						resp.json()?.then(data => {
+								if (data?.rating === 'LIKED') setRating(liked)
+								else if (data?.rating === 'DISLIKED') setRating(disliked)
+								else setRating(notRated)
+							}
+						)
+					}
+				})
+		}
+
+		const getIcons: () => { tu: string, td: string } = () => {
+			if (rating === liked) {
+				return {tu: thumbsUp, td: thumbsDownOutline}
+			} else if (rating === disliked) return {tu: thumbsUpOutline, td: thumbsDown}
+			else return {tu: thumbsUpOutline, td: thumbsDownOutline}
+		}
+
+		const [rating, setRating] = useState(notRated)
+
+
+		const header =
+			<IonHeader>
+				<IonToolbar>
+					<IonButton color="light" fill="solid" routerLink="/">
+						<IonIcon icon={home}/>
+					</IonButton>
+					<IonTitle>{movie.name} ({movie.releaseYear})</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+
+		return (
+			<IonPage>
+				{header}
+				<IonContent fullscreen>
+					<IonCard>
+						<IonCardHeader>
+							<IonCardTitle>{movie.name}</IonCardTitle>
+							<IonCardSubtitle
+								class="card-subtitle">{movie.releaseYear} | {normalizeRuntime(movie.runtimeMin)}
+							</IonCardSubtitle>
+						</IonCardHeader>
+						<IonImg src={movie.posterUrl}/>
+						<IonCardContent>
+							{movie.genres.map((genre, i) => (
+								<IonChip outline key={i}>
+									<IonLabel>{normalizeGenre(genre)}</IonLabel>
+								</IonChip>
+							))}
+							<p>{movie.caption}</p>
+							<IonIcon onClick={() => handleRatingClick(liked)} icon={getIcons().tu} size="large"/>
+							<IonIcon onClick={() => handleRatingClick(disliked)} icon={getIcons().td} size="large"/>
+						</IonCardContent>
+					</IonCard>
+					<IonCard>
+						<IonCardHeader>
+							<IonCardSubtitle>Cast</IonCardSubtitle>
+						</IonCardHeader>
+						<IonCardContent>
+							<IonList class="lst">
+								{movie.cast.map((actor, i) => (
+									<Link to={"/actor/".concat(String(actor.id))} key={i}>
+										<div className="itm">
+											<IonImg src={actor.posterUrl} class="img"/>
+											<IonLabel>{actor.fullName}</IonLabel>
+										</div>
+									</Link>
+								))}
+							</IonList>
+						</IonCardContent>
+					</IonCard>
+					<IonCard>
+						<IonCardHeader>
+							<IonCardSubtitle>Directors</IonCardSubtitle>
+						</IonCardHeader>
+						<IonCardContent>
+							<IonList class="lst">
+								{movie.directors.map((director, i) => (
+									<div className="itm" key={i}>
+										<IonImg src={director.posterUrl} class="img"/>
+										<IonLabel>{director.name}</IonLabel>
+									</div>
+								))}
+							</IonList>
+						</IonCardContent>
+					</IonCard>
+					<IonCard>
+						<IonCardHeader>
+							<IonCardSubtitle>Recommended Movies</IonCardSubtitle>
+						</IonCardHeader>
+						<IonCardContent>
+							<IonList class="lst">
+								{recommended.length ? (recommended.map((mov, i) => (
+									<Link to={"/movie/".concat(String(mov.id))} key={i}>
+										<div className="itm">
+											<IonImg src={mov.posterUrl} class="img"/>
+											<IonLabel>{mov.name}</IonLabel>
+										</div>
+									</Link>
+								))) : <div>No movies found.</div>
+								}
+							</IonList>
+						</IonCardContent>
+					</IonCard>
+				</IonContent>
+			</IonPage>
+		);
+	}
+;
 
 export default MovieDetails;
