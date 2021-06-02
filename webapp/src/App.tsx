cf72322a94fb90a6b5b41be3296e56b71d5cb348
phoenixday <a.tsykynovskaya@gmail.com>
2021-06-02 20:39:24 +0200

    redirect to home, doesn't work

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
@@ -40,6 +40,10 @@ const App: React.FC = () => (
           <ModalLoginTest />
         </Route>
         <Route exact path="/find/:str" component={SearchMovies} />
+        <Route component={Home}/>
+        {/*<Route render={() => <Redirect to={{pathname: "/"}} />} />*/}
+        {/*TODO redirection to home doesn't work*/}
+        {/*<Route render={(match) => <Redirect to="/" />} />*/}
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
